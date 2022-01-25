a50350e697fa5af2efb6a3cd03fd17a884c0e835
salt <732237136@qq.com>
2022-01-25 23:34:29 +0800

    fix: 新的打包参数

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
 /*
  * @Author: Salt
  * @Date: 2022-01-19 22:52:37
- * @LastEditTime: 2022-01-19 23:19:17
+ * @LastEditTime: 2022-01-25 21:18:25
  * @LastEditors: Salt
  * @Description: 入口文件
  */
-
-import modules from './modules'
-
-window.addEventListener('DOMContentLoaded', () => {
-  modules()
-})
-
 /*!
 
 // ==UserScript==
-// @name         New Userscript
+// @name         盐的网页版贴吧体验优化脚本
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  更好的贴吧看帖、管理体验
 // @author       Salt
-// @match        https://tieba.baidu.com/
+// @match        https://tieba.baidu.com/*
 // @icon         https://www.google.com/s2/favicons?domain=baidu.com
 // @grant        none
 // @license      MulanPSL-2.0
 // ==/UserScript==
 
 */
+import modules from './modules'
+
+window.addEventListener('DOMContentLoaded', () => {
+  modules()
+})
